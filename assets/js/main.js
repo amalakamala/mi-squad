468330fc15ffb0669361607832e03b4642b1a10b
amalakamala <amalakamala.a@gmail.com>
2017-05-04 23:00:27 -0300

    agregar squad en array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
+function Squad(nombre,edad,hobbies){
+	this.nombre = nombre;
+	this.edad = edad;
+  	this.hobbies = hobbies;
+}
+
+var arr = [];
+
+var Marcela = new Squad("Marcela Cabello",31,["Cantar", "Yoga", "Cocinar"]);
+arr.push(Marcela);
+
+var Marcelissa = new Squad("Melissa Pacheco",25,["Dormir", "Comer", "Matilda"]);
+arr.push(Marcelissa);
+
+var Paulina = new Squad("Paulina Aros",28,["Hacer libretas", "Acariciar Gatos", "Manicure"]);
+arr.push(Paulina);
+
+var Karla = new Squad("Karla Jeria",35,["Comer Chocolates", "Viajar","Ir al Cine"]);
+arr.push(Karla);
+
+var Tanya = new Squad("Tanya Ramirez",29,["Gatos", "Viajar","Tomar tecito a las 4"]);
+arr.push(Tanya);
+
+var Paula = new Squad("Paula Ponce",28,["Música", "Aves","Heroes of the storm"]);
+arr.push(Paula);
+
+var Mariela = new Squad("Mariela Cubillos",36,["Tejer", "Comer chocolate","Cocinar"]);
+arr.push(Mariela);
+
+var Valentina = new Squad("Valentina Amala Kamala",29,["Bordar", "Tejer","Dormir"]);
+arr.push(Valentina);
 
